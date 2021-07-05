ed7a2bf4aae1bd855b5d7955cbb2b8c750d4ffaa
bharat.mane <bharat.mane@outlook.com>
2021-07-05 08:58:51 +0530

    chors: removed empty files and fixed style issues with classes

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -4,7 +4,7 @@ import { BlogService } from '../services/blog.service';
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.css']
+  
 })
 export class BlogComponent implements OnInit {
  blogPosts:any;
