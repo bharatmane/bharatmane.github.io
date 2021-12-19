f6c8db108ec59eb13fbea2f6484814a2dca90a55
bharat.mane <bharat.mane@outlook.com>
2021-12-19 18:32:09 +0530

    feat: added meta tags for sharing and indexing

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgxMasonryComponent, NgxMasonryOptions } from 'ngx-masonry';
 import { Book } from '../models/book.model';
 import { BOOKS } from '../static/data/books';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-book',
@@ -9,6 +10,8 @@ import { BOOKS } from '../static/data/books';
   styleUrls: ['./book.component.css']
 })
 export class BookComponent implements OnInit {
+  title = 'Bharat Mane: Must Read Books';
+
   allBooks: Book[] = BOOKS;
   public masonryOptions: NgxMasonryOptions = {
     gutter: 20,
@@ -16,7 +19,15 @@ export class BookComponent implements OnInit {
   @ViewChild(NgxMasonryComponent) masonry: NgxMasonryComponent;
 
   
-  constructor() { }
+  constructor(private titleService: Title, private metaService: Meta) {
+    this.titleService.setTitle(this.title);
+    this.metaService.addTags([
+      {name: 'keywords', content: 'Books, Software Craftsmanship, Must Read'},
+      {name: 'description', content: 'This is a crafted list of awesome Software craftsmanship resources to help others learn the craft'},
+      {name: 'og:image', content: 'https://bharatmane.com/assets/images/must-read.jpg'},
+      {name: 'robots', content: 'index, follow'}
+    ]);
+  }
 
   ngOnInit(): void {
 
