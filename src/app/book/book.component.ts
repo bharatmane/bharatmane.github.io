3d94c753ec8a17df238b5916b131e4d1b19db57e
bharat.mane <bharat.mane@outlook.com>
2021-12-22 16:46:47 +0530

    fix: fixed the hotspot vulnerability and added meta tags for book page

--- src/app/book/book.component.ts
+++ src/app/book/book.component.ts
@@ -7,7 +7,7 @@ import { Title, Meta } from '@angular/platform-browser';
 @Component({
   selector: 'app-book',
   templateUrl: './book.component.html',
-  styleUrls: ['./book.component.css']
+  styleUrls: ['./book.component.css'],
 })
 export class BookComponent implements OnInit {
   title = 'Bharat Mane: Must Read Books';
@@ -18,22 +18,48 @@ export class BookComponent implements OnInit {
   };
   @ViewChild(NgxMasonryComponent) masonry: NgxMasonryComponent;
 
-  
   constructor(private titleService: Title, private metaService: Meta) {
     this.titleService.setTitle(this.title);
     this.metaService.addTags([
-      {name: 'keywords', content: 'Books, Software Craftsmanship, Must Read'},
-      {name: 'description', content: 'This is a crafted list of awesome Software craftsmanship resources to help others learn the craft'},
-      {name: 'og:image', content: 'https://bharatmane.com/assets/images/must-read.jpg'},
-      {name: 'robots', content: 'index, follow'}
+      { name: 'keywords', content: 'Books, Software Craftsmanship, Must Read' },
+      {
+        name: 'description',
+        content:
+          'This is a crafted list of awesome Software craftsmanship resources to help others learn the craft',
+      },
+      {
+        name: 'og:description',
+        content:
+          'This is a crafted list of awesome Software craftsmanship resources to help others learn the craft',
+      },
+      {
+        name: 'title',
+        content:
+          'The best wey to understand whats done so far in Software Craftsmanship',
+      },
+      {
+        name: 'og:title',
+        content:
+          'The best wey to understand whats done so far in Software Craftsmanship',
+      },
+      {
+        name: 'og:image',
+        content: 'https://bharatmane.com/assets/images/must-read.jpg',
+      },
+      {
+        name: 'og:url',
+        content: 'https://bharatmane.com/book',
+      },
+      { name: 'robots', content: 'index, follow' },
+      { name: 'article:author', content: 'https://bharatmane.com' },
+      { name: 'author', content: 'Bharat Manecom' },
+  
+  
     ]);
   }
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void { }
   public itemsLoaded() {
     console.log('itemsloaded');
   }
-
 }
